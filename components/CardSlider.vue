b6a3fefabd0188ac6610cae22b77a93a70cfeabe
TreyRuffy <treyruffy@users.noreply.github.com>
2024-02-06 22:19:13 -0700

    A couple improvements

--- components/CardSlider.vue
+++ components/CardSlider.vue
@@ -25,7 +25,7 @@ const cardCount = useDevice().isMobile ? 2 : 3
       :items-to-show="cardCount"
       class="btn-block m-auto max-w-[95vw] rounded-2xl drop-shadow-2xl sm:max-h-[60vh] sm:max-w-[85vw] lg:max-h-[70vh] lg:max-w-[80vw] xl:max-w-[70vw]"
     >
-      <Slide v-for="item in items" :key="item.title" class="rounded-2xl p-2">
+      <Slide v-for="item in items" :key="item.title" class="rounded-2xl p-2 pb-6 pt-4">
         <CategoryCard
           :title="item.title"
           :link="item.link"
@@ -34,6 +34,10 @@ const cardCount = useDevice().isMobile ? 2 : 3
           :new-badge="item.newBadge"
         />
       </Slide>
+      <template #addons="{ slidesCount }">
+        <Navigation v-if="slidesCount > 1" />
+        <Pagination v-if="slidesCount > 1" />
+      </template>
     </Carousel>
   </div>
 </template>
