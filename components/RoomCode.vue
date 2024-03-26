110e681aa9fac277d038ba64082b58a04216f3fa
TreyRuffy <TreyRuffy@users.noreply.github.com>
2024-03-25 19:49:01 -0600

    Player list, top nav

---
+++ components/RoomCode.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  roomCode: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>Room Code: {{ roomCode }}</div>
+</template>
+
+<style scoped></style>
