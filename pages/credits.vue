13acb6dfc055c7fef543ae72f3a3b53b3167d5d1
dbuck087 <dbuck087@gmail.com>
2024-04-14 18:16:09 -0600

    finished credits

--- pages/credits.vue
+++ pages/credits.vue
@@ -5,75 +5,92 @@ import TopNavigation from '~/components/TopNavigation.vue'
 <template>
   <div>
     <TopNavigation />
-    <h1 class="mb-2 mt-4 text-center text-2xl font-semibold">Credits</h1>
-    <div class="m-auto flex justify-center gap-2">
+    <h1 class="m-2 text-center text-[38px] font-bold">Credits</h1>
+    <div class="m-8">
       <div>
-        <h2 class="font-semibold">UI</h2>
-        <ul>
-          <li>
-            <NuxtLink href="https://daisyui.com/" target="_blank"> daisyUI </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://tailwindcss.com/" target="_blank"> Tailwind CSS </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://swiperjs.com/" target="_blank"> Swiper </NuxtLink>
-          </li>
-        </ul>
+        <h2 class="m-auto text-center text-[28px] font-semibold">Libraries</h2>
       </div>
-      <div>
-        <h2 class="font-semibold">Icons</h2>
-        <ul>
-          <li>
-            <NuxtLink href="https://heroicons.com/" target="_blank"> Heroicons </NuxtLink>
-          </li>
-        </ul>
+      <div
+        class="m-auto flex flex-wrap content-stretch items-center justify-center gap-x-8 gap-y-2 text-center sm:gap-y-4"
+      >
+        <div>
+          <NuxtLink href="https://nuxt.com/" target="_blank"> Nuxt </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://vuejs.org/" target="_blank"> Vue </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://pinia.vuejs.org/" target="_blank"> Pinia </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://vueuse.org/" target="_blank"> VueUse </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://vitejs.dev/" target="_blank"> Vite </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://socket.io/" target="_blank"> Socket.IO </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://github.com/unjs/uqr" target="_blank"> uqr </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://github.com/unjs/uncrypto" target="_blank"> uncrypto </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://github.com/leizongmin/js-xss" target="_blank"> xss </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://eslint.org/" target="_blank"> ESLint </NuxtLink>
+        </div>
       </div>
+    </div>
+
+    <div class="m-8">
       <div>
-        <h2 class="font-semibold">Libraries</h2>
-        <ul>
-          <li>
-            <NuxtLink href="https://nuxt.com/" target="_blank"> Nuxt </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://vuejs.org/" target="_blank"> Vue </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://pinia.vuejs.org/" target="_blank"> Pinia </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://vueuse.org/" target="_blank"> VueUse </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://vitejs.dev/" target="_blank"> Vite </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://socket.io/" target="_blank"> Socket.IO </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://github.com/unjs/uqr" target="_blank"> uqr </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://github.com/unjs/uncrypto" target="_blank"> uncrypto </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://github.com/leizongmin/js-xss" target="_blank"> xss </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://eslint.org/" target="_blank"> ESLint </NuxtLink>
-          </li>
-        </ul>
+        <h2 class="m-auto text-center text-[28px] font-semibold">UI / UX</h2>
       </div>
+      <div
+        class="m-auto flex flex-wrap content-stretch items-center justify-center gap-x-8 gap-y-4 text-center"
+      >
+        <div>
+          <NuxtLink href="https://daisyui.com/" target="_blank"> daisyUI </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://tailwindcss.com/" target="_blank"> Tailwind CSS </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://swiperjs.com/" target="_blank"> Swiper </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="m-8">
       <div>
-        <h2 class="font-semibold">APIs</h2>
-        <ul>
-          <li>
-            <NuxtLink href="https://restcountries.com/" target="_blank"> Rest Countries </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink href="https://flagcdn.com/" target="_blank"> Flags API & CDN </NuxtLink>
-          </li>
-        </ul>
+        <h2 class="m-auto text-center text-[28px] font-semibold">APIs</h2>
+      </div>
+      <div
+        class="m-auto flex flex-wrap content-stretch items-center justify-center gap-x-8 gap-y-4 text-center"
+      >
+        <div>
+          <NuxtLink href="https://restcountries.com/" target="_blank"> Rest Countries </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink href="https://flagcdn.com/" target="_blank"> Flags API & CDN </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="m-8">
+      <div>
+        <h2 class="m-auto text-center text-[28px] font-semibold">Icons</h2>
+      </div>
+      <div
+        class="m-auto flex flex-wrap content-stretch items-center justify-center gap-x-8 gap-y-4 text-center"
+      >
+        <div>
+          <NuxtLink href="https://heroicons.com/" target="_blank"> Heroicons </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
