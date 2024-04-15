60dafe540ee68619ed39330d1663e3eb3e9e1f51
AmyJane <amyjane.reilly@gmail.com>
2024-04-15 12:02:40 -0600

    Changes to question page

--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,19 @@
     <div class="flex flex-col items-center gap-2">
       <NuxtLink
         to="/singleplayer"
-        class="btn btn-primary btn-md w-[90vw] text-[17px] md:btn-lg sm:mx-auto sm:max-w-md"
+        class="btn btn-primary btn-md w-[90vw] text-[17px] font-medium shadow-sm md:btn-lg sm:mx-auto sm:max-w-md"
       >
         Single Player
       </NuxtLink>
       <NuxtLink
         to="/create-room"
-        class="btn btn-secondary btn-md w-[90vw] text-[17px] md:btn-lg sm:mx-auto sm:max-w-md"
+        class="btn btn-secondary btn-md w-[90vw] text-[17px] shadow-sm md:btn-lg sm:mx-auto sm:max-w-md"
       >
         Create Room
       </NuxtLink>
       <NuxtLink
         to="/join"
-        class="btn btn-accent btn-md w-[90vw] text-[17px] md:btn-lg sm:mx-auto sm:max-w-md"
+        class="btn btn-accent btn-md w-[90vw] text-[17px] shadow-sm md:btn-lg sm:mx-auto sm:max-w-md"
       >
         Join Room
       </NuxtLink>
--- pages/question.vue
+++ pages/question.vue
@@ -15,20 +15,39 @@ const maxQuestions = 10
       What country does this flag belong to?
     </h1>
     <div class="grid justify-center md:grid-cols-3">
-      <div class="mx-8 flex items-center justify-center">
+      <div class="x-8 flex items-center justify-center">
+        <!-- Timer for bigger screens -->
         <div
-          class="flex h-12 w-12 items-center justify-center rounded-full bg-primary sm:h-24 sm:w-24"
+          class="hidden h-16 w-16 items-center justify-center rounded-full bg-primary shadow-lg md:flex lg:h-24 lg:w-24"
         >
-          120
+          <span class="countdown absolute flex font-mono text-2xl lg:text-4xl">
+            <span style="--value: 41"></span>
+          </span>
         </div>
       </div>
-      <div class="mx-8 flex justify-center">
+      <div class="relative mx-8 flex justify-center">
         <NuxtImg src="https://flagcdn.com/us.svg" format="webp" :height="42" alt="United States" />
+        <!-- Timer for smaller screens -->
+        <div
+          class="absolute start-[-8px] top-[50%] flex h-16 w-16 items-center justify-center rounded-full bg-primary shadow-lg md:hidden"
+        >
+          <span class="countdown flex font-mono text-2xl md:text-4xl">
+            <span style="--value: 40"></span>
+          </span>
+        </div>
       </div>
+
+      <!-- Submitted for larger screens -->
       <div class="mx-8 flex items-center justify-center">
         <h1>Submitted 11/20</h1>
       </div>
-      <h1>The buttons will be here</h1>
+    </div>
+    <!-- The answers are here -->
+    <div class="mx-2 mt-4 grid grid-cols-2 gap-2">
+      <div class="btn btn-primary btn-lg btn-block flex-grow shadow-md">answer 1</div>
+      <div class="btn btn-secondary btn-lg btn-block flex-grow shadow-md">answer 2</div>
+      <div class="btn btn-accent btn-lg btn-block flex-grow shadow-md">answer 3</div>
+      <div class="btn btn-primary btn-lg btn-block flex-grow shadow-md">answer 4</div>
     </div>
   </div>
 </template>
